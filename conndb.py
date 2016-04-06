5bfd05fd8dd1f6f73fa7df956a5b784f2f59af46
MrZhu <876196890@qq.com>
2016-04-06 17:50:54 +0800

    test two

--- conndb.py
+++ conndb.py
@@ -4,11 +4,13 @@ path=db.base.from_this_dir('Mydb.db')
 print(path)
 conn=sqlite3.connect(path)
 print('ok')
+#查询数据库表结构 
 sql="select * from sqlite_master where type='table'"
 cursor=conn.cursor()
 cursor.execute(sql)
 values=cursor.fetchall()
 print(values)
+#空数据库初始化
 if cursor.rowcount <=0 :
     db.base.init_db()
     print('init database has done')
